1664e8193bb2eb54d15ee0df6ea0535b108e947e
PH <oliveiraph17@gmail.com>
2015-05-02 17:28:52 -0300

    Finishes Task B by adding validation and unit tests.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :image_url
+  validates_uniqueness_of :title
+  validates_format_of :image_url, {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.',
+    allow_blank: true
+  }
+  validates_numericality_of :price, greater_than_or_equal_to: 0.01
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title:       products(:programming_ruby).title,
+                          description: "Fred description.",
+                          image_url:   "fred.gif",
+                          price:       1)
+
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+  end
+
+  def new_product(image_url)
+    Product.new(title:       "Title",
+                description: "Description.",
+                image_url:   image_url,
+                price:       1)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png http://a.b/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |url|
+      assert new_product(url).valid?, "#{url} should be valid"
+    end
+
+    bad.each do |url|
+      assert new_product(url).invalid?, "#{url} should not be valid"
+    end
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title:       "Title",
+                          description: "Description.",
+                          image_url:   "xpto.jpg")
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
 end
